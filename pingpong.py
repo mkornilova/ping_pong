2bdbc495bb91dcc618622be6df4495150be6b3fb
mkornilova <166915815+mkornilova@users.noreply.github.com>
2024-04-13 14:36:15 +0300

    Update pingpong.py
    v1.1
    Был добавлен класс, для основного объекта- теннисного мяча. 
    Был добавлен сам теннисный мяч.

--- pingpong.py
+++ pingpong.py
@@ -7,6 +7,20 @@ window = display.set_mode((win_w,win_h))
 display.set_caption('Ping-pong')
 window.fill(back)
 
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+    def __init__(self,imagey,speed,x,y,size_x, size_y ):
+        super().__init__()
+        self.image = transform.scale(image.load(imagey),(size_x,size_y))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+ball = GameSprite('tenis.jpg',4,200,200,80,50)
 clock = time.Clock()
 FPS = 60
 run = True
@@ -14,6 +28,7 @@ while run:
     for e in event.get():
         if e.type == QUIT:
              run = False  
+    ball.reset()
     display.update()
     clock.tick(FPS)
     
