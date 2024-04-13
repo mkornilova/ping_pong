8ce76dbc8b28fc112c23079ef54bafa99d1b79e8
mkornilova <166915815+mkornilova@users.noreply.github.com>
2024-04-13 15:08:57 +0300

    Update pingpong.py
    v1.2
    Добавлены игровые объекты- ракетки.
    Управляются с помощью клавиатуры.
    Левая - W- S
    Возможно передвижение лишь вверх и вниз
    Идентично и правая, но клавиши управления - Вверх, Вниз

--- pingpong.py
+++ pingpong.py
@@ -18,8 +18,22 @@ class GameSprite(sprite.Sprite):
         self.rect.y = y
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_w - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_w - 80:
+            self.rect.y += self.speed                
 
-
+racket1 = Player('racket.png', 4,-35,150,120,200)
+racket2 = Player('racket.png', 4,520,150,120,200)
 ball = GameSprite('tenis.jpg',4,200,200,80,50)
 clock = time.Clock()
 FPS = 60
@@ -27,9 +41,15 @@ run = True
 while run:
     for e in event.get():
         if e.type == QUIT:
-             run = False  
+             run = False
+    window.fill(back)  
     ball.reset()
+    racket1.update_r()
+    racket2.update_l()
+    racket1.reset()
+    racket2.reset()
     display.update()
+    
     clock.tick(FPS)
     
     
